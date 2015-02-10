9228c8cb767deadb8c56758c960b8750e9320344
enkidulan <supamaxy@gmail.com>
2015-02-10 14:31:02 +0200

    Prepare chromedriver 2.14.1.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setupy_download_helper_path = join(
 setupy_download_helper = imp.load_source(
     'setupy_download_helper', setupy_download_helper_path)
 
-version = '2.14'
-setupy_download_helper.CHROMEDRIVER_VERSION = version
+version = '2.14.1'
+setupy_download_helper.CHROMEDRIVER_VERSION = version[:4]
 
 setup(name='chromedriver',
       version=version,
