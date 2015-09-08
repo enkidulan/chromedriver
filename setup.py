ad07602c7923a0d359907488f3f32c1b8d902f05
Maksym Shalenyi <enkidulan@gmail.com>
2015-09-08 17:26:49 +0300

    Prepare chromedriver 2.19.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setupy_download_helper_path = join(
 setupy_download_helper = imp.load_source(
     'setupy_download_helper', setupy_download_helper_path)
 
-version = '2.18'
+version = '2.19'
 setupy_download_helper.CHROMEDRIVER_VERSION = version[:4]
 
 setup(name='chromedriver',
